6e0f3a92f6385bbd2b7f0c62ea07766ff8d31f07
cahlen <cahlen@gmail.com>
2014-12-18 11:39:39 -0500

    Nested loops never fail.

---
+++ problem9.py
@@ -0,0 +1,19 @@
+'''
+Project Euler Problem 9
+
+There exists exactly one pythagorean triple where a < b < c, and a^2+b^2+c^2 = 1000.  Find the product abc.
+
+Created on Dec 18, 2014
+
+@author: cahlen
+'''
+
+for a in range(0,1000):
+    for b in range(0,1000):
+        for c in range(0,1000):
+            # make sure conditions are satisified 
+            if ((a + b + c) == 1000) and (a < b) and (b < c) and (a != 0) and (b != 0) and (c != 0) and ((a**2 + b**2) == c**2):
+                print("a: %d, b: %d, c: %d") %(a,b,c)
+                print("The sum a+b+c = %d") %(a+b+c)
+                print("The product abc = %d") %(a*b*c) # print result
+                break
